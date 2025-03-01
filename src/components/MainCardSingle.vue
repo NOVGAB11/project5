593dbfa1fe3afbeb545a61859cd8e179fe0e9014
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-03-01 12:15:38 -0600

    changed routes to project5

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -10,7 +10,7 @@ const cookingInstructions = faker.lorem.paragraph(4)
 </script>
 
 <template>
-    <RouterLink :to="{ name: 'CardDetail', params: {id: username} }">
+    <RouterLink :to="{ name: 'RecipeDetail', params: {id: recipeName} }">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
             <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'food', height:480, width: 960})" />
             <div class="p-4">
