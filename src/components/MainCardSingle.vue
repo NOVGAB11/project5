40d10c58f97945ce4ca3746c45bd10a6e7a2b1ae
Gabriela Rodriguez <grodri41@stu.southtexascollege.edu>
2025-03-01 12:08:23 -0600

    added faker import and recipe const in script

--- src/components/MainCardSingle.vue
+++ src/components/MainCardSingle.vue
@@ -1,18 +1,18 @@
 <script setup>
+import { faker } from '@faker-js/faker'
 
-    import { faker } from '@faker-js/faker'
-    const firstname = faker.person.firstName()
-    const lastname = faker.person.lastName()
-    const jobtitle = faker.person.jobTitle()
-    const bio = faker.lorem.lines(2)
-const username = faker.internet.userName({firstName: firstname, lastName: lastname}).toLowerCase()
+const recipeName = faker.lorem.words(3); // generating recipe name
+const ingredients = faker.lorem.words(50)
+const timeToCook = faker.datatype.number({ min: 20, max: 60 })
+const cookingInstructions = faker.lorem.paragraph(4)
+ 
 
 </script>
 
 <template>
     <RouterLink :to="{ name: 'CardDetail', params: {id: username} }">
         <div class="border border-gray-500 rounded-lg shadow bg-gray-400 h-full">
-            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'cats', height:480, width: 960})" />
+            <img class="object-fill h-480 w-960 rounded-t-lg" v-bind:src="faker.image.urlLoremFlickr({category: 'food', height:480, width: 960})" />
             <div class="p-4">
                 <p class="mb-2 text-2xl font-bold">{{ firstname }} {{ lastname }} </p>
                 <p class="mb-3 font-normal text-gray-800">{{ jobtitle }} </p>
